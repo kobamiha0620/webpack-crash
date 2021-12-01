4f6070d66f9ecb668ede9810e1c3eb7d35ea382f
kobamiha0620 <miharu.irisawa@twinplanet.co.jp>
2021-12-01 19:27:42 +0900

    import babel

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import _ from 'lodash'
-import './style.css'
-import './style.scss'
-import logo from './logo.png'
-//名前の競合を避けるため
+// import _ from 'lodash'
+// import './style.css'
+// import './style.scss'
+// import logo from './logo.png'
+// //名前の競合を避けるため
 
 
-function component(){
-	const element = document.createElement('div');
-	const array = ['Hello', 'webplackl', '!!!' ]
-	element.innerHTML = _.join(array, ' ')
-	return element;
+// function component(){
+// 	const element = document.createElement('div');
+// 	const array = ['Hello', 'webplackl', '!!!' ]
+// 	element.innerHTML = _.join(array, ' ')
+// 	return element;
+// }
+
+// document.body.appendChild(component());
+// document.body.classList.add('haikei');
+
+// const image = new Image();
+// image.src = logo
+// document.body.appendChild(ima
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render;{
+	<div>Hello, React!</div>
+	document.getElementById('root')
 }
 
-document.body.appendChild(component());
-document.body.classList.add('haikei');
-
-const image = new Image();
-image.src = logo
-document.body.appendChild(image);
+//renderはHTMLをビルドする描画用の関数に
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,73 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 const outputPath = path.resolve(__dirname, 'dist');
 
+
 module.exports = {
- entry:'./src/index.js',
- output: {
-	filename:'main.js',
-	path: outputPath
-},
-module: {
-	rules:[
-		{
-		test:/\.css$/ ,
-		use:[
-		'style-loader',
-		'css-loader'
-		]
-		},
-		{
-			test:/\.scss$/ ,
-			use:[
-			'style-loader',
-			'css-loader',
-			'sass-loader'
-			]
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: outputPath
+	},
+	module: {
+		rules: [{
+				test: /\.m?jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
 			},
-		{
-		test:/\.(jpe?g|png|gif|icon|svg)$/i,
-		loader: 'url-loader',
-		options:{
-		limit: 2048,
-		name: './image/[name].[ext]'		
-}
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.(jpe?g|png|gif|icon|svg)$/i,
+				loader: 'url-loader',
+				options: {
+					limit: 2048,
+					name: './image/[name].[ext]'
+				}
+			},
+
+			{
+				test: /\.html$/,//対象の拡張子を設定
+				loader: 'html-loader'//loaderの設定
+			}
+
+		]
+
+
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "dist"),
 		}
+		//ブラウザに表示する設定 webpack-dev-server
+	},
+	//プラグインの設定
+	plugins:[
+		new HtmlWebpackPlugin({
+			template: './src/index.html',  //雛形ファイル
+			filename: './index.html' //雛形ファイルを最終的に出力先と出力名
+		})
 	]
-
-
-},
-devServer: {       
-    static: {
-		directory: path.join(__dirname, "dist"),
-	  }
-	  //ブラウザに表示する設定 webpack-dev-server
-}
-
+	
 
 }
